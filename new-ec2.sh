54fd83f774e08cccb78ae45823eb31652404d7f3
Yinxiao Zhang <yinxiaoz@amazon.com>
2023-06-21 12:10:27 -0700

    added script for ec2

---
+++ new-ec2.sh
@@ -0,0 +1,15 @@
+sudo yum install docker
+sudo yum install git
+sudo yum install zsh
+
+sudo yum install util-linux-user
+
+sudo passwd ec2-user
+
+chsh -s $(which zsh)
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
