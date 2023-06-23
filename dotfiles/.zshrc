e7dd1de99afa025169addf7e3349c7ea2df90adf
Yinxiao Zhang <yinxiao.zhang@gmail.com>
2023-06-23 06:48:25 +0000

    update dotfile/.zshrc; add more tools into new-ec2.sh

--- dotfiles/.zshrc
+++ dotfiles/.zshrc
@@ -16,7 +16,7 @@ fi
 set -o vi
 
 # Path to your oh-my-zsh installation.
-export ZSH="/Users/yinxiaoz/.oh-my-zsh"
+export ZSH="$HOME/.oh-my-zsh"
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
--- new-ec2.sh
+++ new-ec2.sh
@@ -1,5 +1,32 @@
+sudo yum update
+
+mkdir .vim/backup
+
+# docker
 sudo yum install docker
+sudo usermod -a -G docker ec2-user
+# id ec2-user
+# newgrp docker
+
+# docker-compose
+wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)
+sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
+sudo chmod -v +x /usr/local/bin/docker-compose
+
+# Enable docker service at AMI boot time
+sudo systemctl enable docker.service
+# Start the Docker service
+sudo systemctl start docker.service
+
+# git and github cli
 sudo yum install git
+type -p yum-config-manager >/dev/null || sudo yum install yum-utils
+sudo yum-config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+sudo yum install gh
+
+# auth github
+gh auth login
+
 sudo yum install zsh
 
 sudo yum install util-linux-user
@@ -13,3 +40,45 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# pyenv for python
+curl https://pyenv.run | bash
+
+# echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+# echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+# echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+
+# pyenv virtualenv plugin
+git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+
+# echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
+
+# tmux
+# install deps
+sudo yum install -y gcc kernel-devel make ncurses-devel
+
+# DOWNLOAD SOURCES FOR LIBEVENT AND MAKE AND INSTALL
+curl -LOk https://github.com/libevent/libevent/releases/download/release-2.1.11-stable/libevent-2.1.11-stable.tar.gz
+tar -xf libevent-2.1.11-stable.tar.gz
+cd libevent-2.1.11-stable
+./configure --prefix=/usr/local
+make
+sudo make install
+
+# DOWNLOAD SOURCES FOR TMUX AND MAKE AND INSTALL
+
+curl -LOk https://github.com/tmux/tmux/releases/download/3.0a/tmux-3.0a.tar.gz
+tar -xf tmux-3.0a.tar.gz
+cd tmux-3.0a
+LDFLAGS="-L/usr/local/lib -Wl,-rpath=/usr/local/lib" ./configure --prefix=/usr/local
+make
+sudo make install
+
+# pkill tmux
+# close your terminal window (flushes cached tmux executable)
+# open new shell and check tmux version
+tmux -V
+
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
